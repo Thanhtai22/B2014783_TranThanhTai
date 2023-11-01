a0877c5678d59682e4805c317110ef9bd8bf1369
thanhtai22 <tai127589@gmail.com>
2023-11-02 00:29:35 +0700

    "Hien thi danh sach cac contact lay ve tu server"

---
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+	<h1>add</h1>
+	<ContactForm  :contact="contact"  @create:contact="createCt"/>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+	components: {
+		ContactForm
+	},
+	data() {
+		return {
+			contact: null,
+			message: "",
+
+		};
+	},
+	methods: {
+		async createCt(data){
+			console.log(data)
+			try {
+				this.status = await ContactService.create(data);
+				this.$router.push({ name: "contactbook" });
+
+
+			} catch (error) {
+				console.log(error)
+			}
+		},
+	}
+}
+</script>
